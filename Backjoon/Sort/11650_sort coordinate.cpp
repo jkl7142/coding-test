1485d4d35f526283557989224cbdcdf7deb11599
KimJiHyeok <jkl7142@naver.com>
2021-12-14 05:25:07 +0900

    Upload 11650

---
+++ Backjoon/Sort/11650_sort coordinate.cpp
@@ -0,0 +1,44 @@
+/*
+*   11650번 - 좌표 정렬하기
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+    if (a.first == b.first)
+        return a.second < b.second;
+
+    return a.first < b.first;
+}
+
+void solution(vector<pair<int, int>>& coordinate) {
+    sort(coordinate.begin(), coordinate.end());
+    // sort(coordinate.begin(), coordinate.end(), cmp);
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> coordinate;
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        coordinate.push_back(make_pair(x, y));
+    }
+    
+    solution(coordinate);
+
+    for (auto p : coordinate) {
+        cout << p.first << " " << p.second << "\n";
+    }
+
+    return 0;
+}
