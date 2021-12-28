5159d770a13db97ab5e898e5084bf1e613ccfbc2
KimJiHyeok <jkl7142@naver.com>
2021-12-28 14:21:36 +0900

    Upload 11279

---
+++ Backjoon/Heap/11279_max heap.cpp
@@ -0,0 +1,44 @@
+/*
+*   11279¹ø - ÃÖ´ë Èü
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void solution(vector<int> calculate) {
+    priority_queue<int, vector<int>, less<int>> pq; // ÃÖ´ë Èü
+
+    for (auto x : calculate) {
+        if (x > 0) {
+            pq.push(x);
+        }
+        else if (pq.empty()) {
+            cout << x << "\n";
+        }
+        else {
+            cout << pq.top() << "\n";
+            pq.pop();
+        }
+    }
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+    vector<int> calculate;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        calculate.push_back(x);
+    }
+
+    solution(calculate);
+
+    return 0;
+}
