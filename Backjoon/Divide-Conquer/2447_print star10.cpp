35d1ccb99b6e5406c6f7dda97508f7b9b68061c4
JiHyeok Kim <jkl410907@gmail.com>
2022-03-25 15:29:18 +0900

    Upload 2447

---
+++ Backjoon/Divide-Conquer/2447_print star10.cpp
@@ -0,0 +1,78 @@
+/*
+*   2447번 - 별 찍기 - 10
+*
+*       ***
+*       * *
+*       ***     // N = 3, 9, 27, ...
+*
+*   분할 정복
+*   재귀 함수
+*/
+
+// 풀이 1
+#include <iostream>
+
+using namespace std;
+
+void printStar(int x, int y, int n) {
+    if ((x / n) % 3 == 1 && (y / n) % 3 == 1)
+        cout << " ";
+    else if (n / 3 == 0)
+        cout << "*";
+    else
+        printStar(x, y, n / 3);
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printStar(j, i, n / 3);
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+
+// 풀이 2
+/* 
+#include <iostream>
+
+using namespace std;
+
+void printStar(int i, int j, int n) {
+    if ((i / n) % 3 == 1 && (j / n) % 3 == 1) {
+        cout << " ";
+    }
+    else if (n / 3 == 0) {
+        cout << "*";
+    }
+    else {
+        printStar(i, j, n / 3);
+    }
+}
+
+void recurStar(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printStar(i, j, n);
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    recurStar(n);
+
+    return 0;
+}
+*/
