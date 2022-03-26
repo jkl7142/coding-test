a84ac02d5ed5d222ccd83eaf5a6f702da635772e
JiHyeok Kim <jkl410907@gmail.com>
2022-03-26 23:36:45 +0900

    Upload 15650

--- Backjoon/Backtracking/15650_N and M 2.cpp
+++ Backjoon/Backtracking/15650_N and M 2.cpp
@@ -7,6 +7,46 @@
 
 using namespace std;
 
+// 풀이 1
+
+#define MAX_N 9
+
+int n, m;
+int arr[MAX_N];
+bool visited[MAX_N];
+
+void dfs(int idx, int cur) {
+    if (idx == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = cur; i <= n; i++) {  // cur ~ n까지의 수
+        if (!visited[i]) {
+            arr[idx] = i;
+            visited[i] = true;
+            dfs(idx + 1, i);    // cur를 선택한 i로 변경
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+
+    cin >> n >> m;
+ 
+    dfs(0, 1);
+
+    return 0;
+}
+
+
+// 풀이 2
+
+/* 
 void dfs(int n, int m, int current, int depth, vector<int>& answer, vector<bool>& visited) {
     
     if (depth == m) {
@@ -43,3 +83,4 @@ int main() {
 
     return 0;
 }
+*/
